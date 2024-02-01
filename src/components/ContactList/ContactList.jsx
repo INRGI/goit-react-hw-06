09a999aa88bed93c03fe6691cd1be726254323b7
Ruslan <120032162+INRGI@users.noreply.github.com>
2024-02-01 14:06:40 +0100

    Fix ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,21 +3,22 @@ import { Button, Contact, Container } from './ContactList.styled';
 import { getContacts, getFilters } from '../../redux/selectors';
 import { deleteContact } from '../../redux/contactsSlice';
 
-  
+const getVisibleContacts = (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+};
+    
 const ContactList = () => {
     const dispatch = useDispatch();
     const contacts = useSelector(getContacts);
     const filter = useSelector(getFilters);
-
-    const getVisibleContacts = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-    };
+    const visibleContacts = getVisibleContacts(contacts, filter);
+    
     
     return (
         <Container>
-            {getVisibleContacts.map(contact => (
+            {visibleContacts.map(contact => (
                 <Contact key={contact.id}>
                     {contact.name + ' : ' + contact.number}
                     <Button
